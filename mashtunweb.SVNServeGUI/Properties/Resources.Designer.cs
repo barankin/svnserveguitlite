9c249e7206f62c0cfd9df674d31ea9bde2c816e1
benjamin.rankin@gmail.com <benjamin.rankin@gmail.com@ca65aaae-bcb6-c91e-0b3d-c9d531bceb40>
2011-05-12 19:44:46 +0000

    Added GPL v3 file information

--- mashtunweb.SVNServeGUI/Properties/Resources.Designer.cs
+++ mashtunweb.SVNServeGUI/Properties/Resources.Designer.cs
@@ -7,7 +7,21 @@
 //     the code is regenerated.
 // </auto-generated>
 //------------------------------------------------------------------------------
+/*This file is part of mashtunweb.SVNServeGUILite.
 
+    mashtunweb.SVNServeGUILite is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    mashtunweb.SVNServeGUILite is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with mashtunweb.SVNServeGUILite.  If not, see <http://www.gnu.org/licenses/>.
+ */
 namespace mashtunweb.SVNServeGUILite.Properties {
     using System;
     
